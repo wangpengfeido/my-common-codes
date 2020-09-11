fde7d967184a614450c75fcbb386027b2227fe72
wangpengfeido <wangpengfeido@126.com>
2020-09-11 21:01:13 +0800

    ...

--- 310一些常用东西的实现/100510函数防抖.js
+++ 310一些常用东西的实现/100510函数防抖.js
@@ -1,16 +1,14 @@
-
 /**
  * 函数防抖
  */
 function debounce(func, wait) {
   let timeout;
-  return function () {
-    let context = this;
+  return function (...args) {
     // 清除掉上一次的timeout，重新开始计时
     clearTimeout(timeout);
     timeout = setTimeout(() => {
-      // 保持原来的this
-      func.apply(context, arguments)
-    }, wait)
-  }
+      // 保持调用 this
+      func.apply(this, args);
+    }, wait);
+  };
 }
